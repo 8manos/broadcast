f136e02f8d00a0e1c2f89a89b0ecbd1e8423a350
Anabelle Handdoek <ana@gathacol.net>
2014-01-15 21:24:18 -0500

    agrega algunos calculos de tiempo

--- js/broadcast.js
+++ js/broadcast.js
@@ -1,28 +1,54 @@
 jQuery(document).ready( function($){
-    // Revisamos que horas son
-    function momento(){
-        var d = new Date();
-        var n = d.toUTCString();
-        $('#timecode > span').text( n );
-    }
+    /* Settings */
+    var videos_duracion = 30, /* Segundos */
+        videos_cantidad = 2; 
 
-    setInterval( momento, 1000 ); 
 
     // Lista de clips
-    videos = [
+    clips = [
                 {
                     src: "/video/720_1.mp4",
                     in: 0,
-                    out: 30
+                    out: videos_duracion
                 },
                 {
                     src: "/video/720_2.mp4",
                     in: 0,
-                    out: 30
+                    out: videos_duracion
                 }
             ];
 
-    // Iniciar
+
+    // Revisamos que horas son y en cual clip vamos
+    function momento(){
+        var d             = new Date(),
+            n             = d.toUTCString(),
+            videos        = videos_duracion * videos_cantidad,
+            hoy           = d.getSeconds() + ( 60 * ( d.getMinutes() + ( 60 * d.getHours() ) ) ),
+            veces         = parseFloat(hoy/videos).toFixed(4),
+            momento       = hoy % videos,
+            clip          = Math.floor ( momento / videos_duracion ) + 1,
+            segundos_clip = clip * videos_duracion;
+
+            if( momento < videos_duracion ){
+                segundos_clip = momento;
+            }else{
+                segundos_clip = momento - ( videos_duracion * (clip-1) );
+            }
+
+        $('#timecode > span').text( n );
+        $('#segundos_hoy > span').text( hoy );
+        $('#videos > span').text( videos );
+        $('#veces > span').text( veces );
+        $('#momento > span').text( momento );
+        $('#clip > span').text( clip + " de " + videos_cantidad);
+        $('#segundos_clip > span').text( segundos_clip );
+    }
+
+    setInterval( momento, 1000 ); 
+
+
+    // Iniciamos la reproducción de la secuencia
     loop = 0;
     function init(){
         // console.log( sequence );
@@ -31,7 +57,7 @@ jQuery(document).ready( function($){
         }
         sequence = Popcorn.sequence(
             "broadcast",
-            videos
+            clips
         );
         sequence.listen( 'canplaythrough', sequence.play() );
         sequence.listen( 'ended', init );
