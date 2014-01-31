03baa5fef23a7a2bdc69b9b260686876d3dd0d87
Anabelle Handdoek <ana@gathacol.net>
2014-01-30 19:28:23 -0500

    videos ordenados segun el momento del dia

--- js/broadcast.js
+++ js/broadcast.js
@@ -5,31 +5,17 @@ jQuery(document).ready( function($){
 
 
     // Lista de clips
-    lista_clips = [
-                    {
-                        src: "/video/720_1.mp4"
-                    },
-                    {
-                        src: "/video/720_2.mp4"
-                    }
-                ];
-
-    // Armamos el objeto lista de clips dinamicamente para popcorn
-    clips = [];
-    for( var i in lista_clips ){
-        // Agregamos item vacio al objeto clips
-        clips.push({});
-
-        // Definimos atributos del nuevo item con base en la lista
-        clips[i].src = lista_clips[i].src;
-        clips[i].in = 0;
-        clips[i].out = videos_duracion;
-    }
-
+    lista_clips = new Array(
+                    "/video/720_1.mp4",
+                    "/video/720_2.mp4"
+                  );
 
     // Revisamos que horas son y en cual clip vamos
-    function momento(){
-        var d             = new Date(),
+    var clip = '',
+        segundos_clip = '';
+
+    function momentos(){
+            d             = new Date(),
             n             = d.toUTCString(),
             videos        = videos_duracion * videos_cantidad,
             hoy           = d.getSeconds() + ( 60 * ( d.getMinutes() + ( 60 * d.getUTCHours() ) ) ),
@@ -53,8 +39,27 @@ jQuery(document).ready( function($){
         $('#segundos_clip > span').text( segundos_clip );
     }
 
-    setInterval( momento, 1000 ); 
+    momentos();
+    setInterval( momentos, 1000 ); 
 
+    // Vamos a reordenar la lista de clips segun la hora para simular sincronía.
+    if( clip > 1 ){
+        clips_pasados = lista_clips.slice(0, clip-1);
+        lista_clips.splice( 0, clip-1 );
+        lista_clips = lista_clips.concat( clips_pasados );
+    }
+
+    // Armamos el objeto lista de clips dinamicamente para popcorn
+    clips = [];
+    for( var i in lista_clips ){
+        // Agregamos item vacio al objeto clips
+        clips.push({});
+
+        // Definimos atributos del nuevo item con base en la lista
+        clips[i].src = lista_clips[i];
+        clips[i].in = 0;
+        clips[i].out = videos_duracion;
+    }
 
     // Iniciamos la reproducción de la secuencia
     loop = 0;
